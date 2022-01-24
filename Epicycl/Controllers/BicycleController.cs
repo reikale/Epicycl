4b6d757a6c0fe6672851ac3c61e3fe9ba2b3cd1c
reikale <sviackeviciute@gmail.com>
2022-01-24 18:55:29 +0200

    Some modifications made, installed a bootstrap template

---
+++ Epicycl/Controllers/BicycleController.cs
@@ -0,0 +1,18 @@
+﻿using Epicycl.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epicycl.Controllers
+{
+    public class BicycleController : Controller
+    {
+        // GET: Bicycle/Random
+        public ActionResult Random()
+        {
+            var bike = new Bicycle() { Name = "Ingos dviratis" };
+
+            return View(bike);
+            // return Content("Labas as krabas");
+            // return NotFound();
+        }
+    }
+}
--- Epicycl/Models/Bicycle.cs
+++ Epicycl/Models/Bicycle.cs
@@ -2,5 +2,15 @@
 {
     public class Bicycle
     {
+        public enum Type
+        {
+            City,
+            Montain,
+            Electric,
+            Hybrid
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
     }
 }
